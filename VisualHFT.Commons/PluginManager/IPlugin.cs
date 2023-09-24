fae7b40eda6c6e0fed06e890a77def8b03acbc75
Ariel Silahian <silahian@hotmail.com>
2023-09-24 13:08:55 -0400

    plugin mgr

---
+++ VisualHFT.Commons/PluginManager/IPlugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualHFT.PluginManager
+{
+    public interface IPlugin
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string Description { get; set; }
+
+    }
+}
