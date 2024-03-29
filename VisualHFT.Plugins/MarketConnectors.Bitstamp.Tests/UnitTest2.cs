df2a4123905b7a0db3c86f2500ac719291903964
Serhii Ivanov <ivanovknitting@gmail.com>
2024-03-29 18:00:12 +0200

    Add Bitstamp plugin stub

---
+++ VisualHFT.Plugins/MarketConnectors.Bitstamp.Tests/UnitTest2.cs
@@ -0,0 +1,12 @@
+namespace MarketConnectors.Bitstamp.Tests
+{
+    public class UnitTest2
+    {
+        [Fact]
+        public void Test2()
+        {
+            int a = 42;
+            Assert.Equal(42, a);
+        }
+    }
+}
