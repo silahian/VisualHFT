e5ee6d8ec1a23a3ad246b47c891559d8440e301d
Serhii Ivanov <ivanovknitting@gmail.com>
2024-03-29 15:10:33 +0200

    Add KuCoin plugin stub

---
+++ VisualHFT.Plugins/MarketConnectors.KuCoin.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace MarketConnectors.KuCoin.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int a = 42;
+            Assert.Equal(42, a);
+        }
+    }
+}
---
+++ VisualHFT.Plugins/MarketConnectors.KuCoin/Model/PlugInSettings.cs
@@ -0,0 +1,23 @@
+﻿//using CryptoExchange.Net.CommonObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisualHFT.UserSettings;
+
+namespace MarketConnectors.KuCoin.Model
+{
+    public class PlugInSettings : ISetting
+    {
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+        public List<string> Symbols { get; set; }
+        public int DepthLevels { get; set; }
+        public int UpdateIntervalMs { get; set; }
+
+        public string Symbol { get; set; }
+        public VisualHFT.Model.Provider Provider { get; set; }
+        public AggregationLevel AggregationLevel { get; set; }
+    }
+}
