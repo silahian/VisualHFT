f4d5c71d785ab5bb59fa02694db0c4b7d9c189a1
Serhii Ivanov <ivanovknitting@gmail.com>
2024-03-29 19:46:16 +0200

    Add Kraken plugin stub

---
+++ VisualHFT.Plugins/MarketConnectors.Kraken.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace MarketConnectors.Kraken.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int a = 42;
+            Assert.Equal(42, a);
+        }
+    }
+}
