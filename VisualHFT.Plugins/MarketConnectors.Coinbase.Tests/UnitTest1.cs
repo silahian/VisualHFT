bbb4d44ed30bd52b6ff4659125dad6b4a304c3d7
Serhii Ivanov <ivanovknitting@gmail.com>
2024-03-30 17:05:23 +0200

    Add Coinbase plugin stub

---
+++ VisualHFT.Plugins/MarketConnectors.Coinbase.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace MarketConnectors.Coinbase.Tests
+{
+    public class UnitTest4
+    {
+        [Fact]
+        public void Test1()
+        {
+            int a = 42;
+            Assert.Equal(42, a);
+        }
+    }
+}
