655d7c8ff75e32d432c86c283b0505d5430bbefa
Ariel Silahian <silahian@hotmail.com>
2024-06-25 20:24:38 -0400

    New notification center for a much advanced error handling and user interaction with errors

---
+++ VisualHFT.Commons/Model/ErrorNotification.cs
@@ -0,0 +1,16 @@
+﻿using VisualHFT.Commons.Helpers;
+
+namespace VisualHFT.Commons.Model
+{
+    public class ErrorNotification
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+        public bool IsRead { get; set; }
+        public HelprNorificationManagerCategories Category { get; set; } // Optional: for grouping
+        public HelprNorificationManagerTypes NotificationType { get; set; }
+        public Exception Exception { get; set; }
+    }
+}
